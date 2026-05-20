2af6147bf13d5c52dc0814d5969df84e5b340e23
rahul8879 <rtiwarirahul123@gmail.com>
2026-05-20 07:09:36 +0530

    added the requirements.txt files - ignore to install for now

--- Python/day_01.py
+++ Python/day_01.py
@@ -1,7 +1,1 @@
-age = 24
-if age >= 18:
-    print("You are old enough to vote")
-    print("Have you registered to vote yet?")
-else:
-    print("Sorry, you are too young to vote.")
-    print("Please register to vote as soon as you turn 18!")
+from utils import add
